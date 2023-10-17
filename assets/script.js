d9b3520beabab286ca708e26f5d79ed2be503b68
Dipika Shrestha <dipikashrestha@Dipikas-MacBook-Pro.local>
2023-10-16 20:33:35 -0500

    Fix dropdown

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     // Get references to the dropdown trigger and menu
     var dropdownTrigger = $(".dropdown-trigger");
-    var dropdownMenu = $(".dropdown-menu");
+    var dropdown = $(".dropdown");
 
     // Toggle the dropdown menu when the trigger button is clicked
     dropdownTrigger.click(function () {
-        dropdownMenu.toggleClass("is-active");
+        dropdown.toggleClass("is-active");
     });
 
     // Close the dropdown when clicking outside of it
     $(document).on("click", function (event) {
         if (!$(event.target).closest(".dropdown").length) {
-            dropdownMenu.removeClass("is-active");
+            dropdown.removeClass("is-active");
         }
     });
 });
