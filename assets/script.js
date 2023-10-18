d35592b3d15a166bf46fffab855b3a5acaa59610
Firefox <tshankgg@gmail.com>
2023-10-17 20:08:14 -0500

    manually added Delvin's slideshow in js css and index

--- assets/script.js
+++ assets/script.js
@@ -43,3 +43,30 @@ function getPets() {
     });
 }
 
+//picture slideshow functionality:
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+// Change slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Show the initial slide
+showSlide(currentSlide);
